711588f40d7ae2975fed661253359682abf8562b
Brenda <brenda.luiza.dev@outlook.com>
2026-07-20 22:16:58 -0300

    Ajustando problemas finais ao remover filmes da lista.

--- scripts/listaFilmes.js
+++ scripts/listaFilmes.js
@@ -1,15 +1,16 @@
 import { data } from "./entradas.js";
 import { adicionarLocalStorage, removerLocalStorage } from "./localStorage.js";
 
+
 //Cria estrutura para a lista de filmes adicionados.
 
-function criaDivs(data){
+function criaDivs(nomeFilme, linkPoster){
     
     const elemento = $(
         `<section>
             <div class="lista-filmes__item">
                 <div class="lista-filmes__poster">
-                    <img src="${data.Poster}" alt="${data.Title}">
+                    <img src="${linkPoster}" alt="${nomeFilme}">
                 </div>
                 <div class="lista-filmes__acoes">
                     <button class="lista-filmes__botao-remover">Remover<i class="bi-trash"></i></button>
@@ -40,20 +41,35 @@ function retornaObjeto(nomeFilme, linkImagem){
 
 let listaFilmes = [];
 
+//Adiciona filmes na lista ao recarregar/fechar página.
+
+$(document).on('DOMContentLoaded', ()=>{
+    if(localStorage.length > 0){
+        $("#main__titulo-principal").addClass("filme-modal--oculto");
+    }
+    for(let i = 0; i < localStorage.length; i++){
+        const filmeString = localStorage.getItem(localStorage.key(i));
+        const objetoJsonFilme = JSON.parse(filmeString);
+        const objetoFormatadoFilme = retornaObjeto(objetoJsonFilme.Titulo, objetoJsonFilme.Poster);
+        criaDivs(objetoFormatadoFilme.getTitulo(), objetoFormatadoFilme.getLinkImagem());
+        listaFilmes.push(objetoFormatadoFilme);
+    }
+});
+
 //Adiciona objeto com filme na lista de filmes.
 
 function addNaLista(listaFilmes){
     const objeto = retornaObjeto(data.Title, data.Poster);
     listaFilmes.push(objeto);
     adicionarLocalStorage(objeto.getTitulo(), objeto);
-    criaDivs(data);
+    criaDivs(objeto.getTitulo(), objeto.getLinkImagem());
     $(".filme-modal").addClass("filme-modal--oculto");
 }
 
 //Verifica se o filme existe na lista.
 
 function existeNaLista(listaFilmes, tituloFilme){
-
+    
     for(let i = 0; i < listaFilmes.length; i++){
         if(listaFilmes[i].getTitulo() === tituloFilme) return true;
     }
@@ -62,7 +78,7 @@ function existeNaLista(listaFilmes, tituloFilme){
 
 //Realiza verificações finais e adiciona o filme na lista.
 
-$("#add_filme").on("click", function(){
+$("#add_filme").on("click", () =>{
     
     if(existeNaLista(listaFilmes, data.Title)){
         notie.alert({text: "Esse filme já foi adicionado em sua lista.",
@@ -75,6 +91,7 @@ $("#add_filme").on("click", function(){
             type: "success",
             time: 1.7,
         });
+        $("#main__titulo-principal").addClass("filme-modal--oculto");
     }
 });
 
@@ -82,7 +99,7 @@ $("#add_filme").on("click", function(){
 //Inteligencia para remover filme da lista de filmes
 
 function removeFilmeLista(listaFilmes, nomeFilme){
-
+    
     for(let i = 0; i < listaFilmes.length; i++){
         if(listaFilmes[i].getTitulo() === nomeFilme){
             removerLocalStorage(listaFilmes[i].getTitulo());
@@ -92,11 +109,10 @@ function removeFilmeLista(listaFilmes, nomeFilme){
 }
 
 function verificacoesParaRemocao(listaFilmes, elementoImagem){
-
+    
     const nomeFilme = $(elementoImagem).children().attr("alt");
-
     if(existeNaLista(listaFilmes, nomeFilme)){
-
+        
         removeFilmeLista(listaFilmes, nomeFilme);
         notie.alert({text: "Filme removido com sucesso!",
             type: 'success',
@@ -114,9 +130,12 @@ function verificacoesParaRemocao(listaFilmes, elementoImagem){
 //Inteligencia de remover o elemento da lista de adicionados.
 
 $(document).on("click", ".lista-filmes__botao-remover", function(){
-    const elementoRemover = $(this).parent().parent().parent().parent();
+    const elementoRemover = $(this).parent().parent().parent();
     const imagemElemento = $(this).parent().siblings("div");
     verificacoesParaRemocao(listaFilmes, imagemElemento);
     elementoRemover.remove();
+    if(localStorage.length == 0){
+        $("#main__titulo-principal").removeClass("filme-modal--oculto");
+    }
 });
 
